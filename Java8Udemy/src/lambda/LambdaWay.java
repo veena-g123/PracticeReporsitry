1f84f3511ef8080c535f4b577f3bb7340b8512ed
veenag <veena@virima.com>
2024-07-06 20:44:23 +0530

    Lambda

--- Java8Udemy/src/lambda/LambdaWay.java
+++ Java8Udemy/src/lambda/LambdaWay.java
@@ -2,15 +2,19 @@ package lambda;
 
 public class LambdaWay {
     public static void main(String[] args) {
+        //        Method -1
 //        HelloWorldInterface hwi = new HelloWorldInterface() {
 //            @Override
 //            public void printHellWorld() {
 //                System.out.println("Hello World!");
 //            }
 //        };
-        HelloWorldInterface hwi =()-> {
-           return "Hello world !!!";
-        };
-        System.out.println(hwi.printHellWorld());
+//        Method -2
+//        HelloWorldInterface hwi =()-> {
+//           return "Hello world !!!";
+//        };
+//        Method -3
+        HelloWorldInterface hwi =()->"Hello world !!!";
+                System.out.println(hwi.printHellWorld());
     }
 }
